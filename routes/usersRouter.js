7d31d750e5e97212efaaa546914c6d3fa575a192
Raven <user@users-MacBook-Air.local>
2022-06-11 00:06:59 -0400

    added some stuff

--- routes/usersRouter.js
+++ routes/usersRouter.js
@@ -1,19 +1,51 @@
 const express = require('express')
-//const UserModel = require('../models/usersSchema')
 const UserModel = require('../model/usersSchema')
+// pulls out the two function we need from express validator
+const {check, validationResult, body} = require('express-validator')
+const bcrypt = require('bcrypt')
+const 
+
 // * Create a Router
 const router = express.Router()
 
-//* Create a new User
-router.post('/', async (req, res) => {
+//* Create or Register a new User
+router.post('/', [
+    check('username', "Username is required from Middleware!").notEmpty(),
+    check("email", "Please use a valid email! from middleware").isEmail(),
+    check("password", "Please enter a password").notEmpty(),
+    check("password", "Please enter a password with six or more characters").isLength({min: 6}),
+] ,async (req, res) => {
     const userData = req.body
 
+    const errors = validationResult(req)
+
+    if (!errors.isEmpty()){
+        return res.json(errors.array())
+    }
+
     try {
+        // checking if there is an user with this email in the db
+        const userExist = await UserModel.findOne({email: userData.email})
+        // if user exist we return!
+        if (userExist) {
+            return res.json({msg: "User already exist!"})
+        }
+
+        //* ==== Create New User
+        // 1 Create the salt
+        const SALT = await bcrypt.genSalt(10)
+        // 2 use the salt to create a hash with the user's password
+        const hashedPassword = await bcrypt.hash(userData.password, SALT)
+        // 3 assign the hashed password to the userData
+        userData.password = hashedPassword
+        // Write the user to the db
         const user = await UserModel.create(userData)
+
         res.status(201).json(user)
+        
     } catch (error) {
         console.log(error)
-        res.status(400).json('Bad request!!!!!')
+        res.status(400).json('Bad request!')
     }
 })
 
--- server.js
+++ server.js
@@ -2,10 +2,11 @@
 const express = require('express')
 require('dotenv').config() // init dotenvs
 const morgan = require('morgan')
-
+const helmet = require('helmet')
 const mongoConfig = require('./config/mongoConfig')
 const todosRouter = require('./routes/todoRouter')
 const usersRouter = require('./routes/usersRouter')
+const authRouter = require('./routes/authRouter')
 
 const app = express()
 const PORT = 8000
@@ -13,11 +14,12 @@ const PORT = 8000
 //* middleware
 app.use(express.json())
 app.use(morgan('dev'))
+app.use(helmet())
 
 //* Routers
 app.use('/todos', todosRouter)
 app.use('/users', usersRouter)
-
+app.use('/auth', authRouter)
 
 app.get('/', (req, res) => {
     res.status(200).json("Welcome to my API!")
